12172e275e5bc05f010a168f07edddc8f7e0986d
Aro Arko <aroarko.sd@gmail.com>
2023-03-09 14:20:41 +0800

    global vs scope

--- function.py
+++ function.py
@@ -7,4 +7,17 @@
 #     print("Hello World", i)
 # func(5+2*3)
 
-def practiseFunction()
+# def calcFunction(x, y):
+#     return x - y
+
+# a = 20
+# b = 10
+# differ = calcFunction
+# print(differ(a, b))
+y = 52
+def my_func():
+    global x
+    x=10
+    print("Value inside function: ",y)
+my_func()
+print(x)
